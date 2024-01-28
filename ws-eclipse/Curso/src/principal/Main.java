4c2c75741e3c074f58afe4eb8376188671941df1
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2024-01-28 14:39:50 -0300

    Organizando pastas

--- ws-eclipse/Curso/src/principal/Main.java
+++ ws-eclipse/Curso/src/principal/Main.java
@@ -1,18 +1,15 @@
 package principal;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.*;
-
-import entidade.Triangulo;
-
 
 public class Main {
     public static void main(String[] args) {
-       LocalDate dt = LocalDate.now();
-       LocalTime time = LocalTime.now();
-       
-       System.out.println(dt);
-       System.out.println(time);
+        LocalDate dt = LocalDate.now();
+        LocalTime time = LocalTime.now();
+
+        System.out.println(dt);
+        System.out.println(time);
 
     }
 }
--- ws-eclipse/curso_java/src/curso_java_1/Main.java
+++ ws-eclipse/curso_java/src/curso_java_1/Main.java
@@ -3,11 +3,10 @@ package curso_java_1;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		int x = 35;
 		int y = 35;
-		String Casa = "Ferreira";
-		
+
 		System.out.println(x + y);
 	}
 
